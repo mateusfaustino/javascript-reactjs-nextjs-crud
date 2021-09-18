65744949426249b4fd587bedb7ec8c1732bf609e
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-09-18 16:27:51 -0300

    fix tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center bg-gradient-to-r from-purple-600 to-blue-600
+    `}>
+     <span >Texto aaaaaaaaaaaaaaa</span>
+     <span >Texto aaaaaaaaaaaaaaa</span>
+    </div>
+  )
+}
